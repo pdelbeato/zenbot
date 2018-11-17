91bbc7a9fe1fe5c172cd2d5341d91b4b5f0e903c
Panfilo <Panfilo@192.168.1.104>
2018-11-17 11:00:57 +0100

    Tentativi vari

--- extensions/strategies/bollinger/strategy.js
+++ extensions/strategies/bollinger/strategy.js
@@ -2,6 +2,8 @@ var z = require('zero-fill')
   , n = require('numbro')
   , bollinger = require('../../../lib/bollinger')
   , Phenotypes = require('../../../lib/phenotype')
+  , debug = require('../../../lib/debug')
+  , crypto = require('../../../lib/crypto')
 
 module.exports = {
   name: 'bollinger',
@@ -107,10 +109,13 @@ module.exports = {
   orderExecuted: function (s, type, executeSignal) {
 	  switch(type) {
 	    case 'buy':
-	    	debug.msg('strategy - orderExecuted - last position id = ' + s.my_positions[-1].id)
-	    	executeSignal('sell', undefined, undefined, undefined, undefined, s.my_positions[-1].id)
+	    	debug.msg('strategy - orderExecuted - buy - last position id = ' + s.my_positions[s.my_positions.length-1].id)
+	    	executeSignal('sell', undefined, undefined, undefined, undefined, s.my_positions[s.my_positions.length-1].id)
 	        break;
 	    case 'sell':
+	    	next_id = crypto.randomBytes(4).toString('hex')
+	    	debug.msg('strategy - orderExecuted - sell - next position id = ' + next_id)
+	    	executeSignal('buy', undefined, undefined, false, undefined, next_id)
 	        break;
 	    default:
 	        break
