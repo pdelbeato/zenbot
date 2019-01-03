e7c02929d0afc9287cb95941f7fc4a7c35de6b80
Panfilo <Panfilo@192.168.1.103>
2019-01-03 17:53:26 +0100

    Correzioni

--- extensions/exchanges/gdax/exchange.js
+++ extensions/exchanges/gdax/exchange.js
@@ -429,7 +429,8 @@ module.exports = function gdax (conf) {
 			debug.msg('cancelAllOrders - cancelAllOrders call')
 
 //			client.cancelAllOrders(opts.product_id, function (err, resp, body) {
-			client.cancelOrders(function (err, resp, body) {
+//			client.cancelOrders(function (err, resp, body) {
+			client.cancelAllOrders(function (err, resp, body) {
 				if (err) {
 					debug.msg('cancelAllOrders: err= ')
 					debug.obj('err', err, false)
