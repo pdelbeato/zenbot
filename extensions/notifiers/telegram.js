66fa77405b7ee1dce5e36a4cde1004ebf16f49cf
pdelbeato <pdelbeato@gmail.com>
2019-01-10 12:56:44 +0100

    Correzioni per commit precedente

--- extensions/notifiers/telegram.js
+++ extensions/notifiers/telegram.js
@@ -4,15 +4,27 @@ var TelegramBot = require('node-telegram-bot-api')
 
 module.exports = function telegram (config) {
 	var telegram = {
-			pushMessage: function(title, message, master = false) {
-				var bot = new TelegramBot(config.bot_token)
+		pushMessage: function(title, message) {
+			var bot = new TelegramBot(config.bot_token)
 
-				bot.sendMessage((master ? config.chat_id_master : config.chat_id), title + ': ' + message).catch(function (error) {
-					console.error('\nerror: telegram notification')
+			bot.sendMessage(config.chat_id, title + ': ' + message).catch(function (error) {
+				console.error('\nerror: telegram notification')
 //					console.log(error.response.body) // => { ok: false, error_code: 400, description: 'Bad Request: chat not found' }
+				console.log(error)
+			})
+		}
+
+		pushMessageMaster: function(title, message) {
+			var bot = new TelegramBot(config.bot_token)
+	
+			if (config.chat_id_master) {
+				bot.sendMessage(config.chat_id_master, title + ': ' + message).catch(function (error) {
+					console.error('\nerror: telegram notification')
+	//				console.log(error.response.body) // => { ok: false, error_code: 400, description: 'Bad Request: chat not found' }
 					console.log(error)
 				})
 			}
+		}
+		return telegram
 	}
-	return telegram
 }
