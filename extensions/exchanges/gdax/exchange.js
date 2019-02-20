7bd73e37994d74cbf6be9abb1f157f01cf9bef2e
pdelbeato <pdelbeato@gmail.com>
2019-02-20 22:02:19 +0100

    Correzioni
    - Pezza per la gestione della data per gli ordini presi da exchange

--- extensions/exchanges/gdax/exchange.js
+++ extensions/exchanges/gdax/exchange.js
@@ -310,7 +310,7 @@ module.exports = function gdax (conf) {
 				// force zenbot a order retry; see "engine.js" for possible retry conditions
 				if (reason && reason == 'canceled') {
 					cached_order.status = 'rejected'
-						cached_order.reject_reason = 'post only'
+					cached_order.reject_reason = 'post only'
 				}
 
 			cached_order.done_at = update.time
@@ -921,6 +921,26 @@ module.exports = function gdax (conf) {
 			    executed_value: '0.0000000000000000',
 			    status: 'rejected',
 			    settled: false }
+			    
+			    {
+				    "id": "68e6a28f-ae28-4788-8d4f-5ab4e5e5ae08",
+				    "size": "1.00000000",
+				    "product_id": "BTC-USD",
+				    "side": "buy",
+				    "stp": "dc",
+				    "funds": "9.9750623400000000",
+				    "specified_funds": "10.0000000000000000",
+				    "type": "market",
+				    "post_only": false,
+				    "created_at": "2016-12-08T20:09:05.508883Z",
+				    "done_at": "2016-12-08T20:09:05.527Z",
+				    "done_reason": "filled",
+				    "fill_fees": "0.0249376391550000",
+				    "filled_size": "0.01291771",
+				    "executed_value": "9.9750556620000000",
+				    "status": "done",
+				    "settled": true
+				}
 
 			    websocket
 			    { 	id: update.order_id,
@@ -945,7 +965,8 @@ module.exports = function gdax (conf) {
 								side: body.side,
 								status: body.status,
 								settled: body.settled,
-								filled_size: Number(body.filled_size)
+								filled_size: Number(body.filled_size),
+								done_at: (body.done_at || null)
 							}
 						}
 
