9a5844b963ea68aa72e5f6467e15ca6400406bdb
pdelbeato <pdelbeato@gmail.com>
2019-03-26 22:55:07 +0100

    Correzioni

--- extensions/strategies/bollinger/strategy.js
+++ extensions/strategies/bollinger/strategy.js
@@ -32,58 +32,58 @@ module.exports = {
 	onPeriod: function (s, cb) {
 		if (s.options.strategy.bollinger.data) {
 //			if (s.options.strategy.bollinger.data.upperBound && s.options.strategy.bollinger.data.lowerBound) {
-				let upperBound = s.options.strategy.bollinger.data.upperBound
-				let lowerBound = s.options.strategy.bollinger.data.lowerBound
-				let midBound = s.options.strategy.bollinger.data.midBound
-				let upperBandwidth = (s.options.strategy.bollinger.data.upperBound - s.options.strategy.bollinger.data.midBound)
-				let lowerBandwidth = (s.options.strategy.bollinger.data.midBound - s.options.strategy.bollinger.data.lowerBound)
-				let bandwidth_pct = (upperBound - lowerBound) / midBound * 100
-				let min_bandwidth_pct = s.options.strategy.bollinger.opts.min_bandwidth_pct
-				let upperWatchdogBound = upperBound + (upperBandwidth * s.options.strategy.bollinger.opts.upper_watchdog_pct/100)
-				let lowerWatchdogBound = lowerBound - (lowerBandwidth * s.options.strategy.bollinger.opts.lower_watchdog_pct/100)
-				let upperCalmdownWatchdogBound = upperBound - (upperBandwidth * s.options.strategy.bollinger.opts.calmdown_watchdog_pct/100)
-				let lowerCalmdownWatchdogBound = lowerBound + (lowerBandwidth * s.options.strategy.bollinger.opts.calmdown_watchdog_pct/100)
+			let upperBound = s.options.strategy.bollinger.data.upperBound
+			let lowerBound = s.options.strategy.bollinger.data.lowerBound
+			let midBound = s.options.strategy.bollinger.data.midBound
+			let upperBandwidth = (s.options.strategy.bollinger.data.upperBound - s.options.strategy.bollinger.data.midBound)
+			let lowerBandwidth = (s.options.strategy.bollinger.data.midBound - s.options.strategy.bollinger.data.lowerBound)
+			let bandwidth_pct = (upperBound - lowerBound) / midBound * 100
+			let min_bandwidth_pct = s.options.strategy.bollinger.opts.min_bandwidth_pct
+			let upperWatchdogBound = upperBound + (upperBandwidth * s.options.strategy.bollinger.opts.upper_watchdog_pct/100)
+			let lowerWatchdogBound = lowerBound - (lowerBandwidth * s.options.strategy.bollinger.opts.lower_watchdog_pct/100)
+			let upperCalmdownWatchdogBound = upperBound - (upperBandwidth * s.options.strategy.bollinger.opts.calmdown_watchdog_pct/100)
+			let lowerCalmdownWatchdogBound = lowerBound + (lowerBandwidth * s.options.strategy.bollinger.opts.calmdown_watchdog_pct/100)
 
-				//Controllo la minimum_bandwidth
-				if (min_bandwidth_pct && (bandwidth_pct < min_bandwidth_pct)) {
-//					console.log('bollinger strategy - min_bandwidth_pct= ' + min_bandwidth_pct + ' ; bandwidth_pct= ' + bandwidth_pct)
-					upperBound = midBound * (1 + (min_bandwidth_pct/100)/2)
-					lowerBound = midBound * (1 - (min_bandwidth_pct/100)/2)
-//					console.log('bollinger strategy - nuovi limiti. upperBound ' + upperBound + ' ; lowerBound= ' + lowerBound)
-				}
-				
-				//Se sono attive le opzioni watchdog, controllo se dobbiamo attivare il watchdog
-				if (s.options.pump_watchdog && s.period.close > upperWatchdogBound) {
-					s.signal = 'pump'
+			//Controllo la minimum_bandwidth
+			if (min_bandwidth_pct && (bandwidth_pct < min_bandwidth_pct)) {
+//				console.log('bollinger strategy - min_bandwidth_pct= ' + min_bandwidth_pct + ' ; bandwidth_pct= ' + bandwidth_pct)
+				upperBound = midBound * (1 + (min_bandwidth_pct/100)/2)
+				lowerBound = midBound * (1 - (min_bandwidth_pct/100)/2)
+//				console.log('bollinger strategy - nuovi limiti. upperBound ' + upperBound + ' ; lowerBound= ' + lowerBound)
+			}
+
+			//Se sono attive le opzioni watchdog, controllo se dobbiamo attivare il watchdog
+			if (s.options.pump_watchdog && s.period.close > upperWatchdogBound) {
+				s.signal = 'pump'
 					s.is_pump_watchdog = true
 					s.is_dump_watchdog = false
-				}
-				else if (s.options.dump_watchdog && s.period.close < lowerWatchdogBound) {
-					s.signal = 'dump'
+			}
+			else if (s.options.dump_watchdog && s.period.close < lowerWatchdogBound) {
+				s.signal = 'dump'
 					s.is_pump_watchdog = false
 					s.is_dump_watchdog = true
-				}
+			}
 
-				//Se non siamo in watchdog, utilizza la normale strategia
-				if (!s.is_dump_watchdog && !s.is_pump_watchdog) {
-					if (s.period.close > (upperBound - (upperBandwidth * s.options.strategy.bollinger.opts.upper_bound_pct/100))) {
-						s.eventBus.emit('bollinger', 'sell')
-					}
-					else if (s.period.close < (lowerBound + (lowerBandwidth * s.options.strategy.bollinger.opts.lower_bound_pct/100))) {
-						s.eventBus.emit('bollinger', 'buy')
-					}
-					else {
-						s.signal = null // hold
-					}
+			//Se non siamo in watchdog, utilizza la normale strategia
+			if (!s.is_dump_watchdog && !s.is_pump_watchdog) {
+				if (s.period.close > (upperBound - (upperBandwidth * s.options.strategy.bollinger.opts.upper_bound_pct/100))) {
+					s.eventBus.emit('bollinger', 'sell')
 				}
-				else { //Siamo in watchdog, controlla se ci siamo ancora
-					if (s.period.close > lowerCalmdownWatchdogBound && s.period.close < upperCalmdownWatchdogBound) {
-						s.signal = null
-						s.is_pump_watchdog = false
-						s.is_dump_watchdog = false
-					}
+				else if (s.period.close < (lowerBound + (lowerBandwidth * s.options.strategy.bollinger.opts.lower_bound_pct/100))) {
+					s.eventBus.emit('bollinger', 'buy')
+				}
+				else {
+					s.signal = null // hold
 				}
 			}
+			else { //Siamo in watchdog, controlla se ci siamo ancora
+				if (s.period.close > lowerCalmdownWatchdogBound && s.period.close < upperCalmdownWatchdogBound) {
+					s.signal = null
+					s.is_pump_watchdog = false
+					s.is_dump_watchdog = false
+				}
+			}
+		}
 //		}
 		cb()
 	},
@@ -96,38 +96,38 @@ module.exports = {
 				let lowerBound = s.options.strategy.bollinger.data.lowerBound
 				let midBound = s.options.strategy.bollinger.data.midBound
 				let upperBandwidth = (s.options.strategy.bollinger.data.upperBound - s.options.strategy.bollinger.data.midBound)
-			let lowerBandwidth = (s.options.strategy.bollinger.data.midBound - s.options.strategy.bollinger.data.lowerBound)
-			let bandwidth_pct = (upperBound - lowerBound) / midBound * 100
+				let lowerBandwidth = (s.options.strategy.bollinger.data.midBound - s.options.strategy.bollinger.data.lowerBound)
+				let bandwidth_pct = (upperBound - lowerBound) / midBound * 100
 				let min_bandwidth_pct = s.options.strategy.bollinger.opts.min_bandwidth_pct
-			let upperWatchdogBound = s.options.strategy.bollinger.data.upperBound + (upperBandwidth * s.options.strategy.bollinger.opts.upper_watchdog_pct/100)
-			let lowerWatchdogBound = s.options.strategy.bollinger.data.lowerBound - (lowerBandwidth * s.options.strategy.bollinger.opts.lower_watchdog_pct/100)
+				let upperWatchdogBound = s.options.strategy.bollinger.data.upperBound + (upperBandwidth * s.options.strategy.bollinger.opts.upper_watchdog_pct/100)
+				let lowerWatchdogBound = s.options.strategy.bollinger.data.lowerBound - (lowerBandwidth * s.options.strategy.bollinger.opts.lower_watchdog_pct/100)
 
-			var color_up = 'cyan'
-			var color_down = 'cyan'
-			//Se il prezzo supera un limite del canale, allora il colore del limite è bianco
-			if (s.period.close > (s.options.strategy.bollinger.data.upperBound - (upperBandwidth * s.options.strategy.bollinger.opts.upper_bound_pct/100))) {
-				color_up = 'white'
-			}
-			else if (s.period.close < (s.options.strategy.bollinger.data.lowerBound + (lowerBandwidth * s.options.strategy.bollinger.opts.lower_bound_pct/100))) {
-				color_down = 'white'
-			}
+				var color_up = 'cyan';
+				var color_down = 'cyan';
+				//Se il prezzo supera un limite del canale, allora il colore del limite è bianco
+				if (s.period.close > (s.options.strategy.bollinger.data.upperBound - (upperBandwidth * s.options.strategy.bollinger.opts.upper_bound_pct/100))) {
+					color_up = 'white'
+				}
+				else if (s.period.close < (s.options.strategy.bollinger.data.lowerBound + (lowerBandwidth * s.options.strategy.bollinger.opts.lower_bound_pct/100))) {
+					color_down = 'white'
+				}
 
-			//Ma se siamo in dump/pump, allora il colore del limite è rosso
-			if (s.period.close > upperWatchdogBound) {
-				color_up = 'red'
-			}
-			if (s.period.close < lowerWatchdogBound) {
-				color_down = 'red'
-			}
-			
-			//Controllo la minimum_bandwidth
-			if (min_bandwidth_pct && (bandwidth_pct < min_bandwidth_pct)) {
-				cols.push('*')
-			}
-			
-			cols.push(z(8, n(s.options.strategy.bollinger.data.lowerBound).format('0.00').substring(0,7), ' ')[color_down])
-			cols.push(' <->')
-			cols.push(z(8, n(s.options.strategy.bollinger.data.upperBound).format('0.00').substring(0,7), ' ')[color_up])
+				//Ma se siamo in dump/pump, allora il colore del limite è rosso
+				if (s.period.close > upperWatchdogBound) {
+					color_up = 'red'
+				}
+				if (s.period.close < lowerWatchdogBound) {
+					color_down = 'red'
+				}
+
+				//Controllo la minimum_bandwidth
+				if (min_bandwidth_pct && (bandwidth_pct < min_bandwidth_pct)) {
+					cols.push('*')
+				}
+
+				cols.push(z(8, n(s.options.strategy.bollinger.data.lowerBound).format('0.00').substring(0,7), ' ')[color_down])
+				cols.push(' <->')
+				cols.push(z(8, n(s.options.strategy.bollinger.data.upperBound).format('0.00').substring(0,7), ' ')[color_up])
 			}
 		}
 		else {
